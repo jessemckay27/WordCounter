cd4a76132d4a0ac67dd4a67fb7f6f2ebf146da83
Jesse <rosecity27@comcast.net>
2016-12-05 16:13:25 -0800

    add 3 more tests

--- Tests/WordCounterTests.cs
+++ Tests/WordCounterTests.cs
@@ -7,10 +7,28 @@ namespace WordCounter.Objects
   public class RepeatCounterTest
   {
     [Fact]
-    public void CountRepeats_EnterStringCountOneWord_1()
+    public void CountRepeats_EnterOneWordCountOneWord_1()
+    {
+      RepeatCounter newCounter = new RepeatCounter("snowing", "snowing");
+      Assert.Equal(1, newCounter.CountRepeats());
+    }
+    [Fact]
+    public void CountRepeats_EnterOneWordCountNoWords_0()
+    {
+      RepeatCounter newCounter = new RepeatCounter("raining", "snowing");
+      Assert.Equal(0, newCounter.CountRepeats());
+    }
+    [Fact]
+    public void CountRepeats_EnterMultipleWordsCountOneWord_1()
     {
       RepeatCounter newCounter = new RepeatCounter("it is snowing", "snowing");
       Assert.Equal(1, newCounter.CountRepeats());
+    }
+    [Fact]
+    public void CountRepeats_EnterMultipleWordsCountNoWords_0()
+    {
+      RepeatCounter newCounter = new RepeatCounter("it is raining", "snowing");
+      Assert.Equal(0, newCounter.CountRepeats());
     }
     [Fact]
     public void CountRepeats_EnterStringCountThreeWords_3()
@@ -19,9 +37,9 @@ namespace WordCounter.Objects
       Assert.Equal(3, newCounter.CountRepeats());
     }
     [Fact]
-    public void CountRepeats_EnterStringCountOneWordIgnoreCase_3()
+    public void CountRepeats_EnterStringCountOneWordIgnoreCase_1()
     {
-      RepeatCounter newCounter = new RepeatCounter("it is Snowing on the mountain", "SnOWiNg");
+      RepeatCounter newCounter = new RepeatCounter("it is snowing on the mountain", "SnOWiNg");
       Assert.Equal(1, newCounter.CountRepeats());
     }
   }
